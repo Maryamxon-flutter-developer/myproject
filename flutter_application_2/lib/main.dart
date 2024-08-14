081c2ee27d4e98f6c43427ce927eb303426c13dc
Maryamxon-flutter-developer <maryamxonmeliboyeva@gmail.com>
2024-08-14 15:57:42 +0500

    list kodi

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,125 +1,317 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ListScreen(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<Oddiy> calls = [
+    Oddiy(
+      img:
+          "https://img.freepik.com/fotos-premium/bebe-ojos-grandes-esta-mirando-camara_787273-2554.jpg",
+      name: "Alisa Rodrigez",
+      info: "(3) 15 July, 13:27",
+      icon: Icons.call_sharp,
+      iconColor: const Color.fromARGB(255, 243, 163, 33),
+    ),
+    Oddiy(
+      img:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7twmONO7kDJpQGe6yaZXLi7lQ2AN1GM4N7yfsf5jVYTasdQWFPPIu-P1qhiWW2F9cuOo&usqp=CAU",
+      name: "Pedro",
+      info: "(3) 15 July, 13:27",
+      icon: Icons.add_call,
+      iconColor: Colors.blue,
+    ),
+    Oddiy(
+      img:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs1GdGnaclTwwgLXJa7MgBY6kNGlbS5JzfzM8KYpFVFPrD2KT4kElEEtLhdyEze6_SznE&usqp=CAU",
+      name: "Alisa Rodrigez",
+      info: "(3) 15 July, 13:27",
+      icon: Icons.phone_callback_sharp,
+      iconColor: Color.fromARGB(255, 243, 37, 33),
+    ),
+
+   
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 270,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 19, 3, 42),
+              borderRadius:
+                  const BorderRadius.only(bottomLeft: Radius.circular(100)),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.camera, color: Colors.white, size: 40),
+                    Text("Modychat",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 40)),
+                    Icon(Icons.search, color: Colors.white, size: 40),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 310,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 114, 134, 132),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const ChatsScreen()),
+                                );
+                              },
+                              child: const Text("Chats",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20)),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const StatusScreen()),
+                                );
+                              },
+                              child: Container(
+                                width: 70,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Color.fromARGB(255, 21, 4, 53),
+                                ),
+                                child: Center(
+                                  child: const Text("Status",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20)),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const CallsScreen()),
+                                );
+                              },
+                              child: const Text("Calls",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration:
+                  const BoxDecoration(color: Color.fromARGB(255, 23, 3, 52)),
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 241, 236, 247),
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(100)),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: const [
+                              Text("ALL Calls",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 2, 10, 36),
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w900)),
+                              Padding(
+                                padding: EdgeInsets.only(right: 40),
+                                child: Text("Friends"),
+                              )
+                            ],
+                          ),
+                          const Icon(Icons.menu, size: 30),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: calls.length + (calls.length > 2 ? 1 : 0), // Add 1 for "See More" if there are more than 2 items
+                        itemBuilder: (context, index) {
+                          if (index < calls.length) {
+                            return dizayn(calls[index]);
+                          } else {
+                            return ListTile(
+                              title: Text("See More >", style: TextStyle(color: Color.fromARGB(255, 242, 47, 22), fontSize: 18)),
+                              onTap: () {
+                                // Action for "See More"
+                                print("See More tapped");
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ),
+
+
+
+                      Expanded(
+                      child: ListView.builder(
+                        itemCount: calls.length + (calls.length > 2 ? 1 : 0), // Add 1 for "See More" if there are more than 2 items
+                        itemBuilder: (context, index) {
+                          if (index < calls.length) {
+                            return dizayn(calls[index]);
+                          } else {
+                            return ListTile(
+                              title: Text("See More >", style: TextStyle(color: Color.fromARGB(255, 242, 47, 22), fontSize: 18)),
+                              onTap: () {
+                                // Action for "See More"
+                                print("See More tapped");
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here
+          print("FloatingActionButton Pressed");
+        },
+        backgroundColor: Color.fromARGB(255, 239, 28, 148), // Set background color
+        child: const Icon(Icons.call_sharp, size: 30), // Set the icon
+        elevation: 10.0, // Adjust elevation to moderate
+        // Set shadow color to pink
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Oddiy {
+  String? name;
+  String? info;
+  String? img;
+  IconData? icon;
+  Color? iconColor;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Oddiy({this.name, this.info, this.img, this.icon, this.iconColor});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Widget dizayn(Oddiy ism) {
+  return ListTile(
+    leading: Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        image: DecorationImage(image: NetworkImage(ism.img!), fit: BoxFit.cover),
+      ),
+    ),
+    title: Text(ism.name ?? '',
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+    subtitle: Text(ism.info ?? '',
+        style: const TextStyle(color: Color.fromARGB(255, 104, 101, 101))),
+    trailing: Icon(ism.icon, color: ism.iconColor ?? Colors.orange),
+  );
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ChatsScreen extends StatelessWidget {
+  const ChatsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Chats"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text("This is the Chats Screen"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class StatusScreen extends StatelessWidget {
+  const StatusScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Status"),
+      ),
+      body: const Center(
+        child: Text("This is the Status Screen"),
+      ),
+    );
+  }
+}
+
+class CallsScreen extends StatelessWidget {
+  const CallsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calls"),
+      ),
+      body: const Center(
+        child: Text("This is the Calls Screen"),
+      ),
     );
   }
 }
